ae55fff7801d345fdcb4950aef149a3a753d8c1a
Fernando Pereira <fernandhenriqp@gmail.com>
2017-02-14 13:00:39 -0200

    Screen_update.sh regards for quimera machine

--- i3/screen_update.sh
+++ i3/screen_update.sh
@@ -1,9 +1,16 @@
+THISHOST=$(hostname)
 intern=eDP1
 extern=HDMI-1
 
-if xrandr | grep "$extern disconnected"; then
+
+
+if [ "$THISHOST" == "quimera" ]; then
+    xrandr --output VGA1 --left-of HDMI1
+
+elif xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
+
 else
-    xrandr --output "$extern" --output "HDMI1" --mode 1920x1080 --left-of "eDP1" 
+    xrandr --output "$extern" --output "HDMI1" --mode 1920x1080 --left-of "eDP1"
     xrandr --output "$extern" --output "HDMI-1" --mode 1920x1080 --left-of "eDP-1"
 fi
